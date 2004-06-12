64059cb1731e3c0ef4d5480f32dae6132c876996
herth <herth@baad475a-3a02-0410-b957-fe49c51c92a3>
2004-06-12 13:49:02 +0000

    latest changes

--- trunk/ltk/ltk-remote.lisp
+++ trunk/ltk/ltk-remote.lisp
@@ -1,20 +1,19 @@
 #|
-  Copyright 2003 Peter Herth <herth@peter-herth.de>
+ Ltk-remote  networking support for the Ltk library
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
+ This software is Copyright (c) 2003 Peter Herth <herth@peter-herth.de>
+
+ Peter Herth grants you the rights to distribute
+ and use this software as governed by the terms
+ of the Lisp Lesser GNU Public License
+ (http://opensource.franz.com/preamble.html),
+ known as the LLGPL.
  
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
  
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
 |#
 
 #+:sbcl (require :sb-bsd-sockets)
