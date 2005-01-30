0fe2ef33fa932bbe2881e3f37b54ffda4b11f73c
nobody <nobody@baad475a-3a02-0410-b957-fe49c51c92a3>
2005-01-30 15:55:08 +0000

    This commit was manufactured by cvs2svn to create tag 'ltk-0-8-7-1'.

---
+++ tags/ltk-0-8-7-1/ltk/ltk-mw.lisp
@@ -0,0 +1,228 @@
+#|
+
+ This software is Copyright (c) 2004 Peter Herth <herth@peter-herth.de>
+
+ Peter Herth grants you the rights to distribute
+ and use this software as governed by the terms
+ of the Lisp Lesser GNU Public License
+ (http://opensource.franz.com/preamble.html),
+ known as the LLGPL.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+|#
+
+#|
+
+This is the Ltk megawidgets package. It consists of widgets usable
+for Ltk, written in Lisp/tcl. So wherever the Ltk package runs, this
+extensing package should run as well.
+
+
+Widgets offered are:
+
+o progress
+    A widget displaying a progress bar
+
+o history-entry
+    An entry widget keeping the history of previous input (which can be
+    browsed through with cursor up and down)
+
+   
+|#
+
+(defpackage "LTK-MW"
+  (:use "COMMON-LISP"
+	"LTK"
+	)
+  (:export "PROGRESS"
+	   "PERCENT"
+	   "BAR-COLOR"
+	   "REDRAW-ON-RESIZE"
+	   "HISTORY-ENTRY"
+	   "HISTORY"
+	   "CLEAR-HISTORY"
+	   "MENU-ENTRY"
+	   "APPEND-ITEM"
+	   "DELETE-ITEM"
+	   ))
+
+(in-package :ltk-mw)
+
+
+;;;; mixin class for widget construction
+;;;; for widgets inheriting from redraw-on-resize the generic function
+;;;; redraw is called, whenever the widget is resized (e.g. by window resize)
+;;;;
+
+
+(defgeneric redraw (widget))
+
+
+(defclass redraw-on-resize ()
+  ())
+
+(defmethod initialize-instance :after ((r redraw-on-resize) &key)
+  (bind r "<Configure>" (lambda (evt) (declare (ignore evt))
+			  (redraw r))))
+
+
+;;;; progress bar
+
+(defclass progress (redraw-on-resize canvas)
+  ((rect :accessor rect)
+   (color :accessor bar-color :initarg :color :initform :blue)
+   (percent :accessor percent :initform 0 :initarg :percent)
+   ))
+
+(defmethod redraw ((progress progress))
+  (let ((width (window-width progress))
+	(height (window-height progress)))
+    (set-coords progress (rect progress)
+		(list 0 0 (truncate (* (percent progress) width) 100) height))))
+
+(defmethod initialize-instance :after ((progress progress) &key)
+  (configure progress :borderwidth 2 :relief :sunken)
+  (setf (rect progress) (create-rectangle progress 0 0 0 20))
+  (itemconfigure progress (rect progress) :fill    (bar-color progress))
+  (itemconfigure progress (rect progress) :outline (bar-color progress)))
+
+(defmethod (setf bar-color) :after (val (progress progress))
+  (itemconfigure progress (rect progress) :fill (bar-color progress))
+  (itemconfigure progress (rect progress) :outline (bar-color progress)))
+
+(defmethod (setf percent) :after (val (progress progress))
+  (redraw progress))
+
+
+;;;; history entry widget
+;;;;
+;;;; Entry widget with history of all text entered.
+;;;; 
+
+
+(defclass history-entry (entry)
+  ((history :accessor history :initform (list))
+   (history-pos :accessor history-pos :initform -1)
+   (keepinput :accessor keepinput :initform nil :initarg :keepinput)
+   ))
+
+(defmethod add-history ((entry history-entry) txt)
+  (if (> (length txt) 0)
+      (push txt (history entry)))
+  (setf (history-pos entry) -1))
+
+(defmethod clear-history ((entry history-entry))
+  (setf (history entry) nil)
+  (setf (history-pos entry) -1))
+
+(defmethod initialize-instance :after ((entry history-entry) &key command)
+  
+  (bind entry "<KeyPress-Return>"
+	(lambda (event)
+	  (declare (ignore event))	  
+	  (let ((txt (text entry)))
+	    (add-history entry txt)
+	    (if (keepinput entry)
+		(entry-select entry 0 "end")
+		(setf (text entry) ""))
+	    (ltk::callback (ltk::name entry) (list txt))	   
+	    )))
+  
+  (bind entry "<KeyPress-Up>"
+	(lambda (event)
+	  (declare (ignore event))
+	  (when (< (history-pos entry) (1- (length (history entry))))
+	    (incf (history-pos entry))
+	    (let ((val (nth (history-pos entry) (history entry))))
+	    (when val
+	      (setf (text entry) val)
+		    )))))
+  
+  (bind entry "<KeyPress-Down>"
+	(lambda (event)
+	  (declare (ignore event))					
+	  (if (>= (history-pos entry) 0)
+	      (progn
+		(decf (history-pos entry))
+		(if (>= (history-pos entry) 0)
+		    (setf (text entry) (nth (history-pos entry) (history entry)))
+		  (setf (text entry) "")))
+	    (progn	    
+	      (setf (text entry) "")))))
+
+  (when command (setf (command entry) command))
+  )
+
+(defmethod (setf command) (val (entry history-entry))
+  (ltk::add-callback (ltk::name entry) val))
+
+;;;;
+
+;;;; menu entry
+
+(defclass menu-entry (entry)
+  ((menu :accessor menu)
+   (entries :accessor entries :initform nil))
+  )
+
+(defmethod initialize-instance :after ((entry menu-entry) &key command content)
+  (bind entry "<KeyPress-Return>"
+	(lambda (event)
+	  (declare (ignore event))
+	  (ltk::callback (ltk::name entry) (list (text entry)))))
+
+  (let ((mp (make-menu nil "Popup")))
+    (setf (menu entry) mp)
+    (dolist (c content)
+      (append-item entry c))
+
+    
+    (bind entry "<1>" (lambda (event)
+			(declare (ignore event))
+			(popup mp (+ 3 (window-x entry))  (+ 3 (window-y entry))))))
+  (when command
+    (setf (command entry) command)))
+
+(defmethod (setf command) (val (entry menu-entry))
+  (ltk::add-callback (ltk::name entry) val))
+
+(defgeneric append-item (entry item))
+(defmethod append-item ((entry menu-entry) item)
+  (setf (entries entry) (append (entries entry) (list item)))
+  (make-menubutton (menu entry) item (lambda ()
+				       (setf (text entry) item)			      
+				       (ltk::callback (ltk::name entry) (list item))
+				       
+				       )))
+(defun remove-nth (n list)
+  (concatenate 'list (subseq list 0 n) (subseq list (1+ n))))
+
+(defmethod delete-item ((entry menu-entry) index)
+  (when (< index (length (entries entry)))
+    (setf (entries entry) (remove-nth index (entries entry)))
+    (menu-delete (menu entry) index))
+  )
+
+
+(defun demo ()
+  (with-ltk
+   (let* ((status '(("critical" 10 "red")
+		    ("severe"  20 "orange")
+		    ("normal" 50 "darkgreen")
+		    ))
+	  (f1 (make-instance 'frame))
+	  (lstatus (make-instance 'label :master f1 :text "Status: "))
+	  (bar (make-instance 'progress :master f1))
+	  (f2 (make-instance 'frame))
+	  (entry (make-instance 'menu-entry :master f2 :content (mapcar #'first status)))
+	  )
+     (pack f1 :side :top)
+     (pack lstatus :side :left)
+     (pack bar :side :left)
+     (pack f2 :side :top)
+     (pack entry :side :left)
+     )))
